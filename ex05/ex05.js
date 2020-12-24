c4cd1778ebd7ab583f1c4a8776b2a8995167d9af
freitassp <priettosamuel@gmail.com>
2020-12-24 11:24:48 -0300

    condições

--- ex05/ex05.js
+++ ex05/ex05.js
@@ -1,1 +1,8 @@
-console.log('O console funcionou corretamente!')
+var vel = 65;
+console.log(`A velocidade média do seu carro é ${vel} km/h`)
+
+if(vel > 60) {
+    console.log(`você ultrapassou o limite de velocidade permitida: MULTADO`)
+}
+
+console.log(`Dirija sempre com sinto de segurança`)
